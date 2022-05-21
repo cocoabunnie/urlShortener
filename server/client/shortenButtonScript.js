d487d9727a27129dd7fe2a293ca41fb46c1682bc
duvivierbri <duvivierbri@gmail.com>
2022-05-21 03:40:33 -0400

    Test function in shortenButtonScript.js to ensure it's connected to the input and button

--- server/client/shortenButtonScript.js
+++ server/client/shortenButtonScript.js
@@ -1,3 +1,4 @@
 document.querySelector(".shortenUrlButton").addEventListener("click", function(){
-    alert("Test to see if they're connected :)")
-})
+    var originalURL = document.querySelector(".urlInput").value.trim();
+    console.log(originalURL);
+});
