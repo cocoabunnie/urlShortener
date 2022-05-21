f098774755c0eff43a95f0abd4933c842c37c718
duvivierbri <duvivierbri@gmail.com>
2022-05-21 03:51:30 -0400

    shortenButtonScript.js checks if URL is valid

--- server/client/shortenButtonScript.js
+++ server/client/shortenButtonScript.js
@@ -1,4 +1,10 @@
 document.querySelector(".shortenUrlButton").addEventListener("click", function(){
     var originalURL = document.querySelector(".urlInput").value.trim();
-    console.log(originalURL);
+    
+    //Checking to ensure URL is valid
+    if(originalURL.startsWith("https://") || originalURL.startsWith("http://") || originalURL.startsWith("www.")){
+        alert("Valid URL :)");
+    } else {
+        alert("Not valid...")
+    }
 });
