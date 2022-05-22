cd1c7609c3f65549ee83bfcce02ede7cf4b20dc2
duvivierbri <duvivierbri@gmail.com>
2022-05-22 03:05:53 -0400

    Does not work if URL starts with www. so I added accomodations for this

--- client/shortenButtonScript.js
+++ client/shortenButtonScript.js
@@ -2,6 +2,12 @@ document.querySelector(".shortenUrlButton").addEventListener("click",function(){
 	let originalURL = document.querySelector(".urlInput").value.trim();
 	//Checking if the url is valid, of course
 	if(originalURL.startsWith("http://") || originalURL.startsWith("https://") || originalURL.startsWith("www.")){
+		
+		//Add https:// if the user has the url start with www.
+		if(originalURL.startsWith("www.")){
+			originalURL = "https://" + originalURL;
+		}
+
 		fetch("http://localhost:3000/api/create-url",{
 		method:"POST",
 		body:JSON.stringify({
