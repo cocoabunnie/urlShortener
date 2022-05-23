1d1eba7e3d3592ad119cb8895ea693482e4b7d8d
duvivierbri <duvivierbri@gmail.com>
2022-05-22 20:30:12 -0400

    Link information now displays on analytics screen

--- frontend/src/analytics.js
+++ frontend/src/analytics.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const Analytics = (props) => {
+function Analytics() {
+    const d = [];
+    const baseURL = "http://localhost:5000";
+    const[linkData, setLinkData] = useState([]);
+    
+    useEffect(() => {
+        axios.get(baseURL+"/api/get-data", {}).then(function(response){
+            setLinkData(response.data);
+        }).catch(error => {
+            console.log("Uh oh!" + error);
+        });
+    }, []);
+
+    const renderCards = () => {
+        
+    }
+
     return (
+        
         <div>
             <h1>Welcome to the Link Analytics</h1>
     
             <div className = "dataDisplayContainer">
-                
+                {linkData.map((link) => {
+                    return <h1>{link.url}</h1>
+                })}
             </div>
 
             <a href="/">back</a>
