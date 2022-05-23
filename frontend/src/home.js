35eb102fde1729dc2a4e9fb5264592008df7c5e5
duvivierbri <duvivierbri@gmail.com>
2022-05-23 05:08:06 -0400

    Added CSS for Home.js page

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import axios from 'axios';
+import './cssFiles/Home.css';
 
 const Home = (props) => {
     const baseURL = "http://localhost:5000";
@@ -9,23 +10,30 @@ const Home = (props) => {
     const submitLink = (props) => {
         axios.post(baseURL+"/api/create-url", {
             url: url
-        }).then(() => {
+        }).then((response) => {
             console.log("REACT Add successful!");
+            document.querySelector(".newURLDisplay").innerHTML = `<p>Here's your new link: <a href=${baseURL}/${response.data.shortlink}>${baseURL}/${response.data.shortlink}</a></p>`;
         }).catch((error) => {
             console.log("Uh Oh, error! " + error);
         });
     }
 
     return (
-        <div>
-            <h1 className = "homeHeader">Hello!</h1>
-            <input className="urlInput" type="text" placeholder="Enter long URL here" onChange = {event => setURL(event.target.value)}/>
-            <button className="shortenUrlButton" onClick={() => submitLink()}>Get Short URL</button>
-            <br/>
-            <div className = "newURLDisplay"></div>
-            <br/>
-            <a href="/analytics">Created Links</a>
+        <div className = "test">
+            <div className = "homeScreenContainer">
+                <h1 className = "homeHeader">Welcome To The URL Shortener!</h1>
+                <div className = "searchArea">
+                    <input className="urlInput" type="text" placeholder="Enter long URL here" onChange = {event => setURL(event.target.value)}/>
+                    <button className="shortenUrlButton" onClick={() => submitLink()}>Convert</button>
+                </div>
+                
+                <br/>
+                <div className = "newURLDisplay"></div>
+                <br/>
+                <a href="/analytics">Created Links</a>
+            </div>
         </div>
+        
     );
 
 } 
