57d305fca65d9671aef0d01e22fad3a50f2ad20a
duvivierbri <duvivierbri@gmail.com>
2022-05-23 12:00:02 -0400

    Added a nav bar

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -19,22 +19,31 @@ const Home = (props) => {
     }
 
     return (
-        <div className = "test">
-            <div className = "homeScreenContainer">
-                <h1 className = "homeHeader">Welcome To The URL Shortener!</h1>
-                <div className = "searchArea">
-                    <input className="urlInput" type="text" placeholder="Enter long URL here" onChange = {event => setURL(event.target.value)}/>
-                    <button className="shortenUrlButton" onClick={() => submitLink()}>Convert</button>
-                </div>
-                
-                <br/>
-                <div className = "newURLDisplay"></div>
-                <br/>
-                <a href="/analytics">
-                    <button>Link Analytics</button>
-                </a>
+        <div className="homePageContainer">
+            <title>TEST</title>
+            <div className="navBar">
+                <a href="/analytics">Link Analytics</a>
+                <a href="/instructions">How To Use</a>
             </div>
+
+            <div className = "centeredContainer">
+                <div className = "homeScreenContainer">
+                    <h1 className = "homeHeader">Welcome To The URL Shortener!</h1>
+                    <div className = "searchArea">
+                        <input className="urlInput" type="text" placeholder="Enter long URL here" onChange = {event => setURL(event.target.value)}/>
+                        <button className="shortenUrlButton" onClick={() => submitLink()}>Convert</button>
+                    </div>
+                    
+                    <br/>
+                    <div className = "newURLDisplay"></div>
+                    <br/>
+                    
+                </div>
+            </div>
+
         </div>
+
+        
         
     );
 
